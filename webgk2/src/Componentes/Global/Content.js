3069d112786413e1c3135c163dd57d7178764372
josue cabrera <ultimatejosue9@gmail.com>
2018-09-13 23:32:25 -0600

    Se modifico todo el entorno ya que fallo la app por errores al iniciarla

---
+++ webgk2/src/Componentes/Global/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+          <h1>Contenido</h1>
+          <p>Conentido de la pagina</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
