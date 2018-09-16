775eda516652b8fffabc52f4cc36c44ce90a3dfc
josue cabrera <ultimatejosue9@gmail.com>
2018-09-16 00:26:17 -0600

    cambio no guardado en el ultimo push

--- webgk2/src/Componentes/Crear/index.js
+++ webgk2/src/Componentes/Crear/index.js
@@ -4,7 +4,6 @@ import React, { Component } from 'react';
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
 
-
 var cel = {
     "name": "Iphone X",
     "panta": 6.1,
