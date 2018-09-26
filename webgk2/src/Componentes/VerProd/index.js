7b7a547d50caba682611317b53b518bdfa73d38b
josue cabrera <ultimatejosue9@gmail.com>
2018-09-25 23:22:51 -0600

    Primera Fase de la actualizacion

--- webgk2/src/Componentes/VerProd/index.js
+++ webgk2/src/Componentes/VerProd/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 //Tabla Responsive
 import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import {FormControl} from 'react-bootstrap'
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
 
 
@@ -17,57 +18,91 @@ class VerProd extends Component {
 
         this.ManejadorCambioVP = this.ManejadorCambioVP.bind(this);
         this.ManejadorEliminar = this.ManejadorEliminar.bind(this);
+        this.ManejadorActualizar = this.ManejadorActualizar.bind(this);
+        //this.GuardarActualizar = this.GuardarActualizar.bind(this);
     }
 
-/*--------------------------- ELIMINAR -----------------------------------------------*/
-    //Funcion que maneja los cambios de eventos
+        //Funcion que maneja los cambios de eventos
     //-----------------------------------------
     ManejadorCambioVP(event) {
         this.setState({[event.target.id]: event.target.value});
       }
-
+/*--------------------------- ELIMINAR -----------------------------------------------*/
       //Funcion que realiza la eliminacion
       //-----------------------------------
-     ManejadorEliminar(event,id) {
+      ManejadorEliminar(event,id) {
 
-         //Se elimina el dato del localstorage del ID ingresado
-        // var idG = this.state.idPro;
-         var lista = [];
-         var lista1 = [];
-         var lista2 = [];
-         if (id > 0) {
+        //Se elimina el dato del localstorage del ID ingresado
+       // var idG = this.state.idPro;
+        var lista = [];
+        var lista1 = [];
+        var lista2 = [];
+        if (id > 0) {
 
-             lista = JSON.parse(localStorage.getItem("GKApp"));
-             for (var i = 0; i < lista.length; i++) {
+            lista = JSON.parse(localStorage.getItem("GKApp"));
+            for (var i = 0; i < lista.length; i++) {
 
-                 if (lista[i].idG !== id) {
-                     lista1.push(lista[i]);
-                 }
-             }
+                if (lista[i].idG !== id) {
+                    lista1.push(lista[i]);
+                }
+            }
 
-             //Se parsea la lista y se agrega nuevamente al local storage
-             lista1 = JSON.stringify(lista1);
-             localStorage.setItem('GKApp', lista1);
-             lista2 = JSON.parse(localStorage.getItem("GKApp"));
-             //Se verifica que la lista se encuentre llena
-             if (lista2.length === 0) {
-                 alert('No existen productos');
-             }
-             else {
-                 this.setState({ gklist: lista2 });
-             }
-             event.preventDefault();
-         }
-         else{
-            alert('Ingrese un identificador valido');
-         }
-      }
+            //Se parsea la lista y se agrega nuevamente al local storage
+            lista1 = JSON.stringify(lista1);
+            localStorage.setItem('GKApp', lista1);
+            lista2 = JSON.parse(localStorage.getItem("GKApp"));
+            //Se verifica que la lista se encuentre llena
+            if (lista2.length === 0) {
+                alert('No existen productos');
+            }
+            else {
+                this.setState({ gklist: lista2 });
+            }
+            event.preventDefault();
+        }
+        else{
+           alert('Ingrese un identificador valido');
+        }
+     }
 /*-------------------------------------------------------------------------------*/
 /*------------------------------ Modificar --------------------------------------*/
-    updateInput(key, value) {
-        // update react state
-        this.setState({ [key]: value });
-    }
+   ManejadorActualizar(event, id, nombre, talla, costo, desc) {
+        this.state.idPro = id;
+        this.state.nombrePro = nombre;
+        this.state.tallaPro = talla;
+        this.state.costoPro= costo;
+        this.state.descPro = desc;
+    }/*
+    GuardarActualizar(event, id, nombre, talla, costo, desc){
+        var lista = [];
+        var lista1 = [];
+        var lista2 = [];
+
+        lista = JSON.parse(localStorage.getItem("GKApp"));
+
+       for (var i = 0; i < lista.length; i++) {
+
+            if (lista[i].idG === id) {
+                lista[i].nombre = nombre;
+                lista[i].talla = talla;
+                lista[i].costo = costo;
+                lista[i].Descripcion = desc;
+            }
+            lista1.push(lista[i]);
+        }
+
+        lista1 = JSON.stringify(lista1);
+        localStorage.setItem('GKApp', lista1);
+        lista2 = JSON.parse(localStorage.getItem("GKApp"));
+        //Se verifica que la lista se encuentre llena
+        if (lista2.length === 0) {
+            alert('No existen productos');
+        }
+        else {
+            this.setState({ gklist: lista2 });
+        }
+         //event.preventDefault();
+    }*/
 /*-------------------------------------------------------------------------------*/
 
     //Funcion que maneja la visualizacion de productos
@@ -91,6 +126,32 @@ class VerProd extends Component {
         return(
             <div className="VerProd">
              <h1 className="labelP"> Listado de Productos </h1>
+             
+                <form>
+                    <div>
+                        <label>
+                            ID Producto: <input type="number" maxLength="6" name="Identificador" id="idPro" onChange={(event) => this.ManejadorCambioCr(event)} value={this.state.idPro} disabled/>
+                        </label>
+                        <label >
+                            Nombre: <input type="text" name="Nombre" id="nombrePro" onChange={(event) => this.ManejadorCambioVP(event)} value={this.state.nombrePro} />
+                        </label>
+                        <label >
+                            Talla: <input type="number" name="Talla" id="tallaPro" onChange={(event) => this.ManejadorCambioVP(event)} value={this.state.tallaPro} />
+                        </label>
+                        <label >
+                            Costo: <input type="number" name="Costo" id="costoPro" onChange={(event) => this.ManejadorCambioVP(event)} value={this.state.costoPro} />
+                        </label>
+                        <label >
+                            Descripcion: <textarea type="textarea" name="Descripcion" id="descPro" onChange={(event) => this.ManejadorCambioVP(event)} value={this.state.descPro} />
+                        </label>
+                    </div>
+
+                    <br/>
+                    <div>
+                        <input type="submit" className="colorbtn" value="Guardar Cambios" />
+                    </div>
+                </form>
+                <br/>
                 <Table  border="1">
                     <Thead>
                         <Tr>
@@ -123,7 +184,8 @@ class VerProd extends Component {
                                     {Guantes.Descripcion}
                                 </td>
                                 <td>
-                                    <input type="submit" className="colorbtn" value="Modificar Producto" />
+                                    <input type="submit" className="colorbtn" value="Modificar Producto" 
+                                    onClick={(event) => this.ManejadorActualizar(event,Guantes.idG, Guantes.nombre, Guantes.talla, Guantes.costo, Guantes.Descripcion)} />
                                 </td>
                                 <td>
                                     <input type="submit" className="colorbtn" value="Eliminar Producto" onClick={(event) => this.ManejadorEliminar(event,Guantes.idG)} />
